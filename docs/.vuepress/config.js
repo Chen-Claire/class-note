b73134ce08b09a7acde82936617a2bc396cc8046
Chen-Clair <10835128@fhsh.edu.tw>
2022-03-03 14:47:43 +0800

    init project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const path = require("path");
+
+const mdFile = ".md";
+const rootFolder = path.dirname(__dirname);
+const rootfiles = fs.readdirSync(rootFolder);
+const folders = rootfiles.filter(
+  (item) => path.extname(item) != mdFile && item != ".vuepress"
+);
+
+module.exports = {
+  title: "進階課程筆記",
+  description: "My notes",
+  themeConfig: {
+    logo: "https://www.ttv.com.tw/drama13/shin/images/role_05.jpg",
+    navbar: [...getNavBar()],
+    sidebar: { ...getSideBar() },
+  },
+};
+
+function getNavBar() {
+  const navbar = [];
+  folders.forEach((folder) => {
+    navbar.push({
+      text: folder.toUpperCase(),
+      link: `/${folder}/`,
+    });
+  });
+  return navbar;
+}
+
+function getSideBar() {
+  const sidebar = {};
+  folders.forEach((folder) => {
+    sidebar[`/${folder}/`] = [];
+    const folderFiles = fs.readdirSync(path.join(rootFolder, folder));
+    const children = [];
+    folderFiles
+      .filter((item) => item.toLowerCase() != "readme.md")
+      .forEach((file) => {
+        children.push(`/${folder}/${file}`);
+      });
+    sidebar[`/${folder}/`].push({
+      text: folder.toUpperCase(),
+      children: [`/${folder}/`, ...children],
+    });
+  });
+  return sidebar;
+}
---
+++ docs/README.md
@@ -0,0 +1,10 @@
+---
+home: true
+heroImage: http://up.54fcnr.com/pic_source/e0/84/b5/e084b5d3c3bca4879161dc6362393d73.gif
+heroText: My notes
+tagline: 快速、簡便、容易運作！
+actions:
+  - text: 快速上手
+    link: /blog/
+    type: primary
+---
